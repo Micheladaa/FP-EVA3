41fa36225bda3a2213767ce6b263007af6e2e3e9
Micheladaa <L24550253@chihuahua2.tecnm.mx>
2024-11-04 17:40:01 -0600

    Add files via upload

---
+++ Eva3_1_Metodos/src/eva3_1_metodos/Eva3_1_Metodos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_1_metodos;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva3_1_Metodos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*USO DE LOS METODOS:
+        INVOCACION O LLAMADA*/
+        imprimirMensaje("HOLA MUNDO");
+    }
+    
+    //DECLARACIÓN
+    //HEADER (Encabezado)
+    //Imprimir un mensaje
+    //JAVA: iNICIAR EN MINUSCULARS, VERBO, SIGUIENTES PALABRAS INICIAN EN MAYUSCULAS
+    public static void imprimirMensaje(String mensaje){
+        System.out.println("TU MENSAJE: "+ mensaje);
+    }
+    
+    
+}
